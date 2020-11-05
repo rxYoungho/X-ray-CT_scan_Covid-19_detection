59a68349fa9384e392d43886a0cdf566e346fa22
Youngho Kim <44015667+rxYoungho@users.noreply.github.com>
2020-11-06 01:04:18 +0900

    Add files via upload

---
+++ CT_Scans_detection/Covid(CT)_detection.py
@@ -0,0 +1,62 @@
+import cv2
+import numpy as np
+import time
+
+# Load Yolo
+net = cv2.dnn.readNet("obj_detection/covid4000.weights", "obj_detection/covid_weight.cfg")
+classes = []
+with open("obj_detection/covid.names", "r") as f:
+    classes = [line.strip() for line in f.readlines()]
+layer_names = net.getLayerNames()
+output_layers = [layer_names[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+colors = np.random.uniform(0, 255, size=(len(classes), 3))
+font = cv2.FONT_HERSHEY_PLAIN
+
+frame = cv2.imread('obj_detection/nCT1.jpg')
+height, width, channels = frame.shape
+
+# Detecting objects
+blob = cv2.dnn.blobFromImage(frame, 0.00392, (416, 416), (0, 0, 0), True, crop=False)
+
+net.setInput(blob)
+outs = net.forward(output_layers)
+
+# Showing informations on the screen
+class_ids = []
+confidences = []
+boxes = []
+for out in outs:
+    for detection in out:
+        scores = detection[5:]
+        class_id = np.argmax(scores)
+        confidence = scores[class_id]
+        if confidence > 0.2:
+            # Object detected
+            center_x = int(detection[0] * width)
+            center_y = int(detection[1] * height)
+            w = int(detection[2] * width)
+            h = int(detection[3] * height)
+
+            # Rectangle coordinates
+            x = int(center_x - w / 2)
+            y = int(center_y - h / 2)
+
+            boxes.append([x, y, w, h])
+            confidences.append(float(confidence))
+            class_ids.append(class_id)
+
+indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.4, 0.3)
+
+for i in range(len(boxes)):
+    if i in indexes:
+        x, y, w, h = boxes[i]
+        label = str(classes[class_ids[i]])
+        confidence = confidences[i]
+        color = colors[class_ids[i]]
+        cv2.rectangle(frame, (x, y), (x + w, y + h), color, 2)
+        cv2.rectangle(frame, (x, y), (x + w, y + 30), color, -1)
+        cv2.putText(frame, label + " " + str(round(confidence, 2)), (x, y + 30), font, 3, (255,255,255), 3)
+
+cv2.imshow("Image", frame)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
